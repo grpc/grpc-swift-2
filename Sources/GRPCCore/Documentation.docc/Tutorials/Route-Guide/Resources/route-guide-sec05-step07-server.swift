36c31ceaf4bd8f5ecc774f1ebc9292671f9b1209
VictorPuga <39507381+VictorPuga@users.noreply.github.com>
2026-08-13 09:22:56 -0600

    Align implementation with tutorial

--- Sources/GRPCCore/Documentation.docc/Tutorials/Route-Guide/Resources/route-guide-sec05-step07-server.swift
+++ Sources/GRPCCore/Documentation.docc/Tutorials/Route-Guide/Resources/route-guide-sec05-step07-server.swift
@@ -3,7 +3,7 @@ import GRPCNIOTransportHTTP2
 
 extension RouteGuide {
   func runServer() async throws {
-    let features = try Self.loadFeatures()
+    let features = try self.loadFeatures()
     let routeGuide = RouteGuideService(features: features)
     let server = GRPCServer(
       transport: .http2NIOPosix(
